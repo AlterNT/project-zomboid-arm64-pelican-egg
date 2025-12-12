2be097346075d55b0206809371d2833bec09a1bb
AlterNT <nathanphillis1@gmail.com>
2025-12-12 22:40:02 +0800

    Initial commit
    Update README.md
    
    Create Dockerfile
    
    Create installer Dockerfile
    
    Create runtime Dockerfile
    
    Delete base Dockerfile
    
    Create entrypoint.sh
    
    Update image name
    
    Update Dockerfile

---
+++ README.md
@@ -0,0 +1,7 @@
+# project-zomboid-arm64-pelican-egg
+A pelican egg for Project Zomboid designed to run on ARM64 architectures.
+
+Based on the prior work done by this excellent steam thread.
+https://steamcommunity.com/app/108600/discussions/1/3415433168012191380
+Docker images based off :
+https://github.com/EthanHand/project-zomboid-docker-arm64
---
+++ zomboid/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Based vaguely off what they did in
+# https://github.com/pelican-eggs/yolks
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
